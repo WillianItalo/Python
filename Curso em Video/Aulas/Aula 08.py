53c91b061465cf5368aa435c2b1b82313111c322
Willian Italo <willianitalosilva@gmail.com>
2023-07-24 02:53:20 -0300

    Exercicíos
    Exercícios Curso em Vídeo

---
+++ Curso em Video/Aulas/Aula 08.py
@@ -0,0 +1,12 @@
+#Importando dados , Raiz quadrada .
+#Entrada de dados 
+from math import sqrt
+num = int(input('Digite um número: '))
+
+#Processamento 
+
+raiz = sqrt(num)
+
+#Saida de dados
+
+print('A raíz de {} é igual a {:.2f}'.format(num, raiz))
---
+++ Curso em Video/Aulas/Aula 08b.py
@@ -0,0 +1,11 @@
+#Importando dados, Random 
+#Entrada de dados 
+import random
+import emoji   
+#Processamento 
+
+num = random.randint(1, 10)
+
+#Saida de dados
+
+print (num,emoji.emojize(':snake:'))
