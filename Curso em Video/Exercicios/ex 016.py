a4f6c027dc4b0c4f2320e6a971656351a9c1906b
Willian Italo <willianitalosilva@gmail.com>
2023-07-25 11:53:12 -0300

    Exercícios
    Exercícios Curso em Vídeo

--- Curso em Video/Exercicios/ex 016.py
+++ Curso em Video/Exercicios/ex 016.py
@@ -1,12 +1,9 @@
-#
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 #Entrada de dados 
-
-
-
-
+from math import trunc
+num = float(input('Digite um valor: ' ))
 
 #Processamento 
 
-
-
 #Saida de dados
+print('O valor Digitado foi {} e a sua parte inteira é {}'.format(num, trunc(num)))
