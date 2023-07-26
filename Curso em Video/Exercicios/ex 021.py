73a0270dee915e8d2f26777dab4c40fc46119db1
Willian Italo <willianitalosilva@gmail.com>
2023-07-26 12:14:36 -0300

    Exercícios
    Exercícios Curso em Vídeo

---
+++ Curso em Video/Exercicios/ex 021.py
@@ -0,0 +1,8 @@
+#Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+#Entrada de dados 
+import pygame
+
+#Processamento 
+
+#Saida de dados
+print('O valor Digitado foi {} e a sua parte inteira é {}'.format(num, trunc(num))) 
