7e87682246f079ed08f7547a1b3c9e2f46173df7
Willian Italo <willianitalosilva@gmail.com>
2023-07-28 11:47:18 -0300

    Exercícios
    Exercícios Curso em Vídeo

--- Curso em Video/Exercicios/ex 021.py
+++ Curso em Video/Exercicios/ex 021.py
@@ -1,8 +1,11 @@
 #Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 #Entrada de dados 
 import pygame
+pygame.init()
+pygame.mixer.music.load('Joker.wav')
 
 #Processamento 
+pygame.mexer.music.play()
 
 #Saida de dados
-print('O valor Digitado foi {} e a sua parte inteira é {}'.format(num, trunc(num))) 
+pygame.event.wait()
