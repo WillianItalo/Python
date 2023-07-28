4b646bb523eecf6ea2ff93e86373faa4bcb19afe
Willian Italo <116980661+WillianItalo@users.noreply.github.com>
2023-07-27 21:26:23 -0300

    Exercícios

---
+++ Curso em Video/Aulas/Aula 09.py
@@ -0,0 +1,11 @@
+#Entrada de dados 
+frase = ('Curso em Vídeo Python')
+
+#Saida de dados
+print(frase.split())
+print(frase[4])
+print(frase.replace('Python','Android'))
+print(frase.upper().count('O'))
+print(len(frase))
+print('Curso' in frase)
+print(frase.find('em'))
