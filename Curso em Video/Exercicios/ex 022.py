753636d779f1096c0cf40db59084390cdd505404
Willian Italo <willianitalosilva@gmail.com>
2023-07-28 11:23:39 -0300

    Exercícios
    Exercício Curso em Vídeo

--- Curso em Video/Exercicios/ex 022.py
+++ Curso em Video/Exercicios/ex 022.py
@@ -1,9 +1,18 @@
 #Crie um programa que leia o nome completo de uma pessoa e mostre :
 #Nome com todas letras maiuscula , com todas as letras minusculas, quantas letras ao todo, quantas letras tem o primeiro nome 
 #Entrada de dados 
-nome = input('Digite o seu nome completo  : ')
+nome = input('Digite o seu nome completo  : ').strip()
 
 #Processamento 
 
 
 #Saida de dados
+
+print('Seu nome em maiúsculas : \n{} '.format(nome.upper()))
+print('Seu nome em minúsculas : \n{}'.format(nome.lower()))
+print('Seu nome te ao todo {} letras '.format(len(nome) - nome.count(' ')))
+#print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
+
+separa = nome.split
+
+print('Seu primeiro nome é {} , e ele tem {} letras'.format(separa[0], len(separa[0])))
