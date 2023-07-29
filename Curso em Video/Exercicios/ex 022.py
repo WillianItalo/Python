ca7690339c8c79ed6b68bbe64711871712293288
Willian Italo <willianitalosilva@gmail.com>
2023-07-29 01:34:53 -0300

    Exercícios
    Exercícios Curso em Vídeo

--- Curso em Video/Exercicios/ex 022.py
+++ Curso em Video/Exercicios/ex 022.py
@@ -11,8 +11,4 @@ nome = input('Digite o seu nome completo  : ').strip()
 print('Seu nome em maiúsculas : \n{} '.format(nome.upper()))
 print('Seu nome em minúsculas : \n{}'.format(nome.lower()))
 print('Seu nome te ao todo {} letras '.format(len(nome) - nome.count(' ')))
-#print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
-
-separa = nome.split
-
-print('Seu primeiro nome é {} , e ele tem {} letras'.format(separa[0], len(separa[0])))
+print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
--- Curso em Video/Exercicios/ex 023.py
+++ Curso em Video/Exercicios/ex 023.py
@@ -1,8 +1,16 @@
 #Faça um programa que leia um número de 0 a 9999 e motre na tela cada um dos dígitos separados. 
 #Entrada de dados 
-
+num = int(input("Digite um numero : "))
 
 #Processamento 
- 
+u = num // 1 % 10
+d = num // 10 % 10 
+c = num // 100 % 10 
+m = num // 1000 % 10
 
 #Saida de dados
+print("Analisando o número {}".format(num))
+print("Unidade: {}".format(u))
+print("Dezena: {}".format(d))
+print("Centena: {}".format(c))
+print("Milhar: {}".format(m))
