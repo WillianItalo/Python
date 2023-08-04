8b1391cba46f90d88cf2e7da7352217ba9652c77
Willian Italo <116980661+WillianItalo@users.noreply.github.com>
2023-08-03 23:07:00 -0300

    Exercícios

--- Curso em Video/Aulas/Aula 10.py
+++ Curso em Video/Aulas/Aula 10.py
@@ -1,12 +1,14 @@
-#
 #Entrada de dados 
-
-
-
+n1 = float(input("Digite a sua primeira nota : " ))
+n2 = float(input("Digite a sua segunda nota : "))
 
 
 #Processamento 
-
-
+res = (n1+n2)/2
+if res >= 6:
+  print("Sua média foi boa Parabéns !!!")
+else:
+  print("Sua média foi ruim ! Estude mais !! ")
 
 #Saida de dados
+print ("Sua média foi {:.1f}".format(res))
