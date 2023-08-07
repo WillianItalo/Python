1fb48a6b7a9b7ac3ac6531eed0f7898a445458dd
Willian Italo <116980661+WillianItalo@users.noreply.github.com>
2023-08-07 11:07:00 -0300

    Exercícios

---
+++ Curso em Video/Aulas/Aula 12.py
@@ -0,0 +1,17 @@
+#Aula 12 IF , Elif & Else.
+#Entrada de dados 
+nome = str(input("Qual o seu nome ? : \n"))
+cores = {"azul":"\033[4;30;44m}
+
+#Processamento 
+if nome == "Joao" or  nome == "Maria" or nome == "Lucas":
+    print("Seu nome é populr no Brasil !")
+elif nome == "Marcus" or nome == "Ana":
+    print("Um nome comum")
+elif nome == "Rose":
+    print("Que nome lindo")
+else:
+    print("Seu nome é bem normal.")
+
+#Saida de dados
+print("Tenha um ótimo dia, {}{}!".format(cores[azul] , nome))
