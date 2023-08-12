4419fc1553a11d7a2a731a9e8a70fb9090d3c4e6
Willian Italo <116980661+WillianItalo@users.noreply.github.com>
2023-08-11 21:54:38 -0300

    Exercícios

---
+++ Curso em Video/Exercicios/ex 036.py
@@ -0,0 +1,9 @@
+#Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte  o  valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+#A prestação ,emsal, não pode execer 30% do salário ou então o emprestimo será negado.
+
+#Entrada de dados 
+
+
+#Processamento 
+
+#Saida de dados
