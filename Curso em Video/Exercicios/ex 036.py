c43c88892d0a4322740f961e0ba17ffde15f1049
Willian Italo <willianitalosilva@gmail.com>
2023-09-23 18:13:41 -0300

    Update ex 036.py

--- Curso em Video/Exercicios/ex 036.py
+++ Curso em Video/Exercicios/ex 036.py
@@ -1,9 +1,14 @@
 #Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte  o  valor da casa, o salário do comprador e em quantos anos ele vai pagar.
-#A prestação ,emsal, não pode execer 30% do salário ou então o emprestimo será negado.
+#A prestação mensal, não pode execer 30% do salário ou então o emprestimo será negado.
 
 #Entrada de dados 
-
+print("---" * 12)
+valcasa = float(input("Digite o valor da casa : "))
+salario = float(input("Digite o seu salário : "))
+parcelas = int(input("Em quantas parcelas deseja "))
 
 #Processamento 
+
+valoraprovado = salario - (salario * 30 / 100)
 
 #Saida de dados
